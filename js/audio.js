e4ed090e191cae70dc7e33f7327652144765f797
Gayan Sachintha <118958174+Gayan-Sachintha@users.noreply.github.com>
2023-11-10 00:44:56 +0530

    background music
    added some background music and button click tones

---
+++ js/audio.js
@@ -0,0 +1,59 @@
+//for index 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const startbtn = document.getElementById("startbtn");
+    const tone = new Audio("../assets/audio/goThrough.mp3");
+
+    startbtn.addEventListener("click", function () {
+        tone.play();
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const muteButton = document.getElementById("mutebtn");
+    const backgroundAudio = new Audio("../assets/audio/bg_music.mp3"); 
+    let isMuted = false;
+
+    
+    muteButton.addEventListener("click", function() {
+        if (isMuted) {
+            backgroundAudio.loop = true;
+            backgroundAudio.play();
+            muteButton.innerHTML = '<i class="bi bi-volume-up-fill"></i>';
+        } else {
+            backgroundAudio.loop = false;
+            backgroundAudio.pause();
+            muteButton.innerHTML = '<i class="bi bi-volume-mute-fill"></i>';
+        }
+        isMuted = !isMuted;
+    });
+});
+
+//for login
+
+document.addEventListener("DOMContentLoaded", function () {
+    const loginbtn = document.getElementById("loginbtn");
+    const regbtn = document.getElementById("regbtn");
+
+    const tone = new Audio("../assets/audio/goThrough.mp3");
+
+    loginbtn.addEventListener("click", function () {
+        tone.play();
+    });
+
+    regbtn.addEventListener("click", function () {
+        tone.play();
+    });
+});
+
+
+//for register
+
+document.addEventListener("DOMContentLoaded", function () {
+    const startbtn = document.getElementById("cancelbtn");
+    const tone = new Audio("../assets/audio/goThrough.mp3");
+
+    startbtn.addEventListener("click", function () {
+        tone.play();
+    });
+});
